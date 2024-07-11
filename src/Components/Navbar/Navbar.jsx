c17743568ff0db1c9d934679434ca23994ff6120
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-07-11 15:35:38 +0530

    add transition effect in nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import iitp_logo from '../../assets/iitp-logo.png'
 
 
 const Navbar = () => {
+
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(()=>{
+      window.addEventListener('scroll', ()=>{
+        window.scrollY > 50 ? setSticky(true) : setSticky(false);
+      })
+    },[])
+
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
       <img src={iitp_logo} alt="logo" className='logo'/>
       <ul>
         <li>Home</li>
