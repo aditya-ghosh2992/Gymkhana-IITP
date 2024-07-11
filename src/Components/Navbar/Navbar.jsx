edfbf4cb5a39ed8ad1d2bf1a181d8ddc9120f3c9
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-07-11 11:06:21 +0530

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
       <img src={logo} alt="" className='logo'/>
       <ul>
         <li>Home</li>
-        <li>Program</li>
         <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
+        <li>Clubs</li>
+        <li>Events</li>
+        <li>Teams</li>
         
-        <li><button className='btn'>Contact us</button></li>
+        <li><button className='btn'>Register</button></li>
       </ul>
     </nav>
   )
