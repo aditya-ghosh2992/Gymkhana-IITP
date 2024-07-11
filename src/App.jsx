ef06ca8a62af9eac3f5d7f90185ffa31a29a7572
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-07-11 16:03:20 +0530

    add new events section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Events from './Components/Events/Events'
 
 
 const App = () => {
@@ -15,6 +16,9 @@ const App = () => {
         <Title subTitle='Our PROGRAM' title='What We Offer'/>
         <Programs/>
         <About/>
+        <Title subTitle='Events' title='Campus Photos'/>
+        <Events/>
+        <Title subTitle='Clubs' title='Explore The Interest'/>
       </div>
       
     </div>
